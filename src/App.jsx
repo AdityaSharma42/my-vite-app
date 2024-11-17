8ecdec64201801ac3768bafe7b47313555a68855
AdityaSharma42 <adityasharma3@usf.edu>
2024-11-16 22:42:25 -0500

    Create ProductList component to render each ProductItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import React, {useState} from 'react';
 import ProductList from './ProductList';
-import AddProductForm from './AddProductForm';
+import AddProductForm from './AddProductForm'; //importing the necessary components
 
 function App(){
-  const [products, setProducts]= useState([
+  const [products, setProducts]= useState([ 
     {id: 1, name: 'Book on Flags', price: '$40', description: 'A book with information about the flags of the world' },
     {id: 2, name: 'Map', price: '$60', description: 'A 48x72 world map to hang in your room' },
     {id: 3, name: 'Globe', price: '$70', description: 'A nice globe  ' },
   ]);
 
-const [nextId, setNextId]= useState(4);
-const addProduct= (newProduct)=> {
+const [nextId, setNextId]= useState(4); // I have listed 3 products, so when entering the next product it will start at 4
+const addProduct= (newProduct)=> {// the function adds a new product
   setProducts([...products, newProduct]);
-  setNextId(nextId+1);
+  setNextId(nextId+1); //the next product that is added will have an id of 5 and so on
 };
 
 return(
   <div>
-    <h1>Product Dashboard</h1>
-    <AddProductForm addProduct= {addProduct}/>
-    <ProductList products= {products}/>
+    <h1>Products</h1>
+    <AddProductForm addProduct= {addProduct}/> {/*addProduct is passed as a prop to AddProductForm to add products*/}
+    <ProductList products= {products}/>{/*products is passed as a prop to ProductList to display the products*/}
   </div>
 );
 }
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ProductItem from './ProductItem';
 
-function ProductList ({products}){
+function ProductList ({products}){ //'products' prop is accepted by ProductList and it displays the products
     return (
        <div>
         <h2> Products </h2>
         <ul>
             {products.map((product)=>(
-                <ProductItem key={product.id} product= {product}/>
+                <ProductItem key={product.id} product= {product}/> //The id is used as the key prop
 
             ))}
         </ul>
        </div> 
     );
 }
-export default ProductList;
+export default ProductList; //exporting ProductList
